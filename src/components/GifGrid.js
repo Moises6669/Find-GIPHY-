15588ccea723265d16fb78e76187f9da2cdf4648
moises6669 <moisesalex132@gmail.com>
2022-01-28 07:19:13 -0600

    ➕ADD: useeffect for render app

--- src/components/GifGrid.js
+++ src/components/GifGrid.js
@@ -1,9 +1,36 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
 
-export const GifGrid = ({category}) => {
-    return (
-        <div>
-            <h3>{category}</h3>
-        </div>
-    )
-}
+export const GifGrid = ({ category }) => {
+  const [count, setcount] = useState(0);
+
+  useEffect(() => {
+    getGif();
+  }, []);
+
+  const getGif = async () => {
+    const url =
+      "https://api.giphy.com/v1/gifs/search?api_key=hFwW2HfDVVe5nSppQiVNxTYNRHZ3262h&q=Goku&limit=10";
+
+    const resp = await fetch(url);
+
+    const { data } = await resp.json();
+
+    const gifs = data.map((img) => {
+      return {
+        id: img.id,
+        title: img.title,
+        url: img.images?.downsized_medium.url,
+      };
+    });
+
+    console.log(gifs);
+  };
+
+  return (
+    <div>
+      <p>{category}</p>
+      <h3>{count}</h3>
+      <button onClick={() => setcount(count + 1)}></button>
+    </div>
+  );
+};
